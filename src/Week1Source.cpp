5a31e139bf6ddc778df7e4d28a296738c4b89894
zepar-web <radu.robert1101@gmail.com>
2022-02-23 17:45:36 +0200

    sap1 work-status

--- src/Week1Source.cpp
+++ src/Week1Source.cpp
@@ -1,19 +1,21 @@
 #include "Week1Header.h"
 
-int Sum(int a, float b) { return a - b; }
-int Dif(char a, int b) { return a / b; }
-int Mul(long a, int b) { return a + b; }
-char Div(int a, int b) { return a * b; }
+int Sum(int a, int b) { return a + b; }
+int Dif(int a, int b) { return a - b; }
+int Mul(int a, int b) { return a * b; }
+int Div(int a, int b) { return a / b; }
 
 void ParseSources()
 {
-	char input[7] = "---H***E+++L+++L///O---P+++O/+-**O---";
-	func Operatori[4] = {Sum, Dif, 65, Mul, 0, Div};
-	int S, V;
-	Content x = 15;
-	double idx;
+	char input[38] = "---H***E+++L+++L///O---P+++O/+-**O---";
+	func Operatori[4] = {Sum, Dif, Mul, Div};
+	int S = 0, V;
+	Content x;
+	x.p1 = 0;
+	x.p2 = 0;
+	int idx;
 
-	for (i = 0; i < strlen(input); i++)
+	for (int i = 0; i < strlen(input); i++)
 	{
 		switch (input[i] - 42)
 		{
@@ -21,25 +23,29 @@ void ParseSources()
 				idx = 2;
 				x.p1 = 3;
 				x.p2 = 3;
+				break;
 			case SUMA:
 				idx = 0;
 				x.p1 = 7;
 				x.p2 = 5;
+				break;
 			case DIFERENTA:
 				idx = 1;
 				x.p1 = 10;
 				x.p2 = 1;
+				break;
 			case IMPARTIRE:
 				idx = 3;
 				x.p1 = 8;
 				x.p2 = 4;
+				break;
 		}
 
-		S = S + Operatori[idx](x.p1, x.p2);
+		 S = S + Operatori[idx](x.p1, x.p2);
 	}
 
 	//S=337
-	printf("S = %c\n", S);
+	printf("S = %d\n", S);
 }
 
 // Week 1 - using C API
