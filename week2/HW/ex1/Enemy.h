e1a6ebff6852a890b15d6ba29af4b6c4b8400e1a
zepar-web <radu.robert1101@gmail.com>
2022-03-06 18:15:43 +0200

    week2 status

--- week2/HW/ex1/Enemy.h
+++ week2/HW/ex1/Enemy.h
@@ -9,9 +9,14 @@ private:
     int health;
 
 public:
+    int get_Health(); // get method for 'health'  attribute 
+    void set_health(int new_val); // set method for 'health' attribute 
+    
     void Init(Point intitialLocation, int initialHealh);
+
     void Move(EnemyDirection direction, int step);
 
     bool IsDead();//returns true is current health is lower or equal to 0
+
     void Shoot(int damage); //decrease the health by damage
 };
